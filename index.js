34f5f3299f3ef0b373248c88ae32d36e2f57bca7
su-tran75 <su-tran@hotmail.fr>
2021-11-29 18:04:29 +0800

    Creating User Router and User Model

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -21,6 +23,10 @@ app.use(morgan("common"));
  * res.send("Welcome to homepage");
  * });
  */
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
 app.listen(8800, () => {
   console.log("Backend server is running!");
 });
